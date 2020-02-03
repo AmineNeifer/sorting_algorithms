7f15b383a9eb78dce4942417de50fbae49fd112a
amineneifer <amineneifer2000@gmail.com>
2020-02-03 14:00:09 +0000

    selection scort task done

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * selection_sort - sort an array.
+ *
+ * @array: array to sort.
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int min, tmp, test = 0;
+	size_t i, j, min_index;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		test = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				test++;
+				min = array[j];
+				min_index = j;
+			}
+		}
+		if (test != 0)
+		{
+			tmp = array[i];
+			array[i] = min;
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
+	}
+}
