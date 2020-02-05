2ec2005ec610de426e0f583de5b97e26c011dca9
emna7 <bhmemna7@gmail.com>
2020-02-05 12:49:09 +0000

    adding comments

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include "sort.h"
 /**
-*
-*
-*
+* swap - function that swaps elements in array
+* @array: array to modify
+* @i: int
+* @j: int
+* Return: nothing
 */
 void swap(int *array, int i, int j)
 {
@@ -14,9 +16,12 @@ array[i] = array[j];
 array[j] = tmp;
 }
 /**
-*
-*
-*
+* partition - lomuto partition scheme
+* @array: array to deal with
+* @high: highest index
+* @low: the first index of the array
+* @size: size of the array
+* Return: integer
 */
 int partition(int *array, int low, int high, size_t size)
 {
@@ -37,15 +42,17 @@ i++;
 }
 if (array[i] != array[high])
 {
-swap(array, i , high);
+swap(array, i, high);
 print_array(array, size);
 }
 return (i);
 }
 /**
-*
-*
-*
+ * q_sort - recursive function that  does the quick sort method.
+ * @array: the array
+ * @low: the first index of giving array.
+ * @high: highest index of giving array.
+ * @size: size of the total array.
 **/
 void q_sort(int *array, int low, int high, size_t size)
 {
@@ -57,6 +64,11 @@ q_sort(array, low, pivot - 1, size);
 q_sort(array, pivot + 1, high, size);
 }
 }
+/**
+* quick_sort - function that sorts an array of integers in ascending order
+* @array: array to sort
+* @size: size of the array
+*/
 void quick_sort(int *array, size_t size)
 {
 q_sort(array, 0, size - 1, size);
